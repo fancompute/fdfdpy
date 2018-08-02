b08929b3ce95cc48c3072b86c836de95345dfed7
Ian Williamson <ian.williamson@utexas.edu>
2018-08-02 15:59:53 -0700

    Moved PML functions into pml.py

--- FDFD/linalg.py
+++ FDFD/linalg.py
@@ -9,6 +9,7 @@ import numpy as np
 from time import time
 
 from FDFD.constants import *
+from FDFD.pml import S_create
 
 def dL(N, xrange, yrange=None):
 	# solves for the grid spacing
@@ -119,41 +120,6 @@ def construct_A(omega, xrange, yrange, eps_r, NPML, pol,
 	return (A, derivs, dAdeps)
 
 
-def sig_w(l, dw, m=4, lnR=-12):
-	# helper for S()
-
-	sig_max = -(m+1)*lnR/(2*ETA_0*dw)
-	return sig_max*(l/dw)**m
-
-
-def S(l, dw, omega):
-	# helper for create_sfactor()
-
-	return 1-1j*sig_w(l, dw)/(omega*EPSILON_0)
-
-
-def create_sfactor(wrange, s, omega, Nw, Nw_pml):
-	# used to help construct the S matrices for the PML creation
-
-	sfactor_array = np.ones(Nw, dtype=np.complex128)
-	if Nw_pml < 1:
-		return sfactor_array
-	hw = np.diff(wrange)[0]/Nw
-	dw = Nw_pml*hw
-	for i in range(0, Nw):
-		if s is 'f':
-			if i <= Nw_pml:
-				sfactor_array[i] = S(hw * (Nw_pml - i + 0.5), dw, omega)
-			elif i > Nw - Nw_pml:
-				sfactor_array[i] = S(hw * (i - (Nw - Nw_pml) - 0.5), dw, omega)
-		if s is 'b':
-			if i <= Nw_pml:
-				sfactor_array[i] = S(hw * (Nw_pml - i + 1), dw, omega)
-			elif i > Nw - Nw_pml:
-				sfactor_array[i] = S(hw * (i - (Nw - Nw_pml) - 1), dw, omega)
-	return sfactor_array
-
-
 def createDws(w, s, dL, N, matrix_format=DEFAULT_MATRIX_FORMAT):
 	# creates the derivative matrices
 
@@ -180,54 +146,6 @@ def createDws(w, s, dL, N, matrix_format=DEFAULT_MATRIX_FORMAT):
 			dyb = sp.diags([1, -1, -1], [0, -1, Ny-1], shape=(Ny, Ny))
 			Dws = 1/dy*sp.kron(dyb, sp.eye(Nx), format=matrix_format)
 	return Dws
-
-
-def S_create(omega, N, Npml, xrange, yrange=None, matrix_format=DEFAULT_MATRIX_FORMAT):
-	# creates S matrices for the PML creation
-
-	M = np.prod(N)
-	if np.isscalar(Npml): Npml = np.array([Npml])
-	if len(N) < 2:
-		N = append(N,1)
-		Npml = append(Npml,0)
-	Nx = N[0]
-	Nx_pml = Npml[0]
-	Ny = N[1]
-	Ny_pml = Npml[1]
-
-	# Create the sfactor in each direction and for 'f' and 'b'
-	s_vector_x_f = create_sfactor(xrange, 'f', omega, Nx, Nx_pml)
-	s_vector_x_b = create_sfactor(xrange, 'b', omega, Nx, Nx_pml)
-	s_vector_y_f = create_sfactor(yrange, 'f', omega, Ny, Ny_pml)
-	s_vector_y_b = create_sfactor(yrange, 'b', omega, Ny, Ny_pml)
-
-	# Fill the 2D space with layers of appropriate s-factors
-	Sx_f_2D = np.zeros(N, dtype=np.complex128)
-	Sx_b_2D = np.zeros(N, dtype=np.complex128)
-	Sy_f_2D = np.zeros(N, dtype=np.complex128)
-	Sy_b_2D = np.zeros(N, dtype=np.complex128)
-
-	for i in range(0, Nx):
-		Sy_f_2D[:, i] = 1/s_vector_y_f
-		Sy_b_2D[:, i] = 1/s_vector_y_b
-
-	for j in range(0, Ny):
-		Sx_f_2D[j, :] = 1/s_vector_x_f
-		Sx_b_2D[j, :] = 1/s_vector_x_b
-
-	# Reshape the 2D s-factors into a 1D s-array
-	Sx_f_vec = Sx_f_2D.reshape((-1,))
-	Sx_b_vec = Sx_b_2D.reshape((-1,))
-	Sy_f_vec = Sy_f_2D.reshape((-1,))
-	Sy_b_vec = Sy_b_2D.reshape((-1,))
-
-	# Construct the 1D total s-array into a diagonal matrix
-	Sx_f = sp.spdiags(Sx_f_vec, 0, M, M, format=matrix_format)
-	Sx_b = sp.spdiags(Sx_b_vec, 0, M, M, format=matrix_format)
-	Sy_f = sp.spdiags(Sy_f_vec, 0, M, M, format=matrix_format)
-	Sy_b = sp.spdiags(Sy_b_vec, 0, M, M, format=matrix_format)
-
-	return (Sx_f, Sx_b, Sy_f, Sy_b)
 
 
 def unpack_derivs(derivs):
---
+++ FDFD/pml.py
@@ -0,0 +1,86 @@
+from numpy import array, ones, zeros, diff, prod, isscalar, complex128
+from scipy.sparse import spdiags
+from FDFD.constants import *
+
+
+def _sig_w(l, dw, m=4, lnR=-12):
+	# helper for S()
+
+	sig_max = -(m+1)*lnR/(2*ETA_0*dw)
+	return sig_max*(l/dw)**m
+
+
+def _S(l, dw, omega):
+	# helper for create_sfactor()
+
+	return 1 - 1j*_sig_w(l, dw)/(omega*EPSILON_0)
+
+
+def _create_sfactor(wrange, s, omega, Nw, Nw_pml):
+	# used to help construct the S matrices for the PML creation
+
+	sfactor_array = ones(Nw, dtype=complex128)
+	if Nw_pml < 1:
+		return sfactor_array
+	hw = diff(wrange)[0]/Nw
+	dw = Nw_pml*hw
+	for i in range(0, Nw):
+		if s is 'f':
+			if i <= Nw_pml:
+				sfactor_array[i] = _S(hw * (Nw_pml - i + 0.5), dw, omega)
+			elif i > Nw - Nw_pml:
+				sfactor_array[i] = _S(hw * (i - (Nw - Nw_pml) - 0.5), dw, omega)
+		if s is 'b':
+			if i <= Nw_pml:
+				sfactor_array[i] = _S(hw * (Nw_pml - i + 1), dw, omega)
+			elif i > Nw - Nw_pml:
+				sfactor_array[i] = _S(hw * (i - (Nw - Nw_pml) - 1), dw, omega)
+	return sfactor_array
+
+
+def S_create(omega, N, Npml, xrange, yrange=None, matrix_format=DEFAULT_MATRIX_FORMAT):
+	# creates S matrices for the PML creation
+
+	M = prod(N)
+	if isscalar(Npml): Npml = array([Npml])
+	if len(N) < 2:
+		N = append(N,1)
+		Npml = append(Npml,0)
+	Nx = N[0]
+	Nx_pml = Npml[0]
+	Ny = N[1]
+	Ny_pml = Npml[1]
+
+	# Create the sfactor in each direction and for 'f' and 'b'
+	s_vector_x_f = _create_sfactor(xrange, 'f', omega, Nx, Nx_pml)
+	s_vector_x_b = _create_sfactor(xrange, 'b', omega, Nx, Nx_pml)
+	s_vector_y_f = _create_sfactor(yrange, 'f', omega, Ny, Ny_pml)
+	s_vector_y_b = _create_sfactor(yrange, 'b', omega, Ny, Ny_pml)
+
+	# Fill the 2D space with layers of appropriate s-factors
+	Sx_f_2D = zeros(N, dtype=complex128)
+	Sx_b_2D = zeros(N, dtype=complex128)
+	Sy_f_2D = zeros(N, dtype=complex128)
+	Sy_b_2D = zeros(N, dtype=complex128)
+
+	for i in range(0, Nx):
+		Sy_f_2D[:, i] = 1/s_vector_y_f
+		Sy_b_2D[:, i] = 1/s_vector_y_b
+
+	for j in range(0, Ny):
+		Sx_f_2D[j, :] = 1/s_vector_x_f
+		Sx_b_2D[j, :] = 1/s_vector_x_b
+
+	# Reshape the 2D s-factors into a 1D s-array
+	Sx_f_vec = Sx_f_2D.reshape((-1,))
+	Sx_b_vec = Sx_b_2D.reshape((-1,))
+	Sy_f_vec = Sy_f_2D.reshape((-1,))
+	Sy_b_vec = Sy_b_2D.reshape((-1,))
+
+	# Construct the 1D total s-array into a diagonal matrix
+	Sx_f = spdiags(Sx_f_vec, 0, M, M, format=matrix_format)
+	Sx_b = spdiags(Sx_b_vec, 0, M, M, format=matrix_format)
+	Sy_f = spdiags(Sy_f_vec, 0, M, M, format=matrix_format)
+	Sy_b = spdiags(Sy_b_vec, 0, M, M, format=matrix_format)
+
+	return (Sx_f, Sx_b, Sy_f, Sy_b)
