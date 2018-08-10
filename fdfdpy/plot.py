35850475b7372a7bc43b2e4199d880f16735872c
Ian Williamson <iwill@stanford.edu>
2018-08-09 20:22:06 -0700

    Updated examples notebook Added feature to generate field animations

--- fdfdpy/plot.py
+++ fdfdpy/plot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 def plt_base(field_val, outline_val, cmap, vmin, vmax, label, cbar=True, outline=None, ax=None):
     # Base plotting function for fields
@@ -42,3 +43,28 @@ def plt_base_eps(field_val, outline_val, cmap, vmin, vmax, cbar=True, outline=No
     ax.set_yticks([])
 
     return ax
+
+
+def plt_base_ani(field_val, cbar=True, Nframes=40, interval=80):
+
+    fig, ax = plt.subplots(1, constrained_layout=True)
+    h = ax.imshow(np.zeros(field_val.shape).transpose(), origin='lower')
+    
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    def init():
+        vmax=np.abs(field_val).max()
+        h.set_data(np.zeros(field_val.shape).transpose())
+        h.set_cmap('RdBu')
+        h.set_clim(vmin=-vmax, vmax=+vmax)
+        
+        return (h,)
+
+    def animate(i):
+        h.set_data(np.real(field_val*np.exp(1j*2*np.pi*i/(Nframes-1))).transpose())
+        return (h,)
+    
+    plt.close()
+    return animation.FuncAnimation(fig, animate, init_func=init, 
+                                    frames=Nframes, interval=interval, blit=True)
