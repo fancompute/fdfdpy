a874ff4a66ab335516be0ce6a7efda53968b3a0a
twhughes <tylerwhughes91@gmail.com>
2018-09-16 14:57:09 +0200

    installable through 'python setup.py install

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='fdfdpy',
+    version='0.1.0',
+    description='Electromagnetic Finite Difference Frequency Domain Solver',
+    long_description=readme,
+    author='Tyler Hughes, Momchil Minkov, Ian Williamson',
+    author_email='twhughes@stanford.edu',
+    url='https://github.com/fancompute/fdfdpy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')) + ['fdfdpy/pyMKL']
+)
