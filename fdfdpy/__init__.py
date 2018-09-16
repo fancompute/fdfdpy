e4b69bae41b6f6cb15ad5a9dcea7afef90abe217
twhughes <tylerwhughes91@gmail.com>
2018-09-16 16:23:11 +0200

    unittest

--- fdfdpy/__init__.py
+++ fdfdpy/__init__.py
@@ -1,3 +1,5 @@
+name = "fdfdpy"
+
 # This line makes it possible to load Simulation object directly as
 # `from fdfdpy import Simulation`
 from .simulation import Simulation
--- setup.py
+++ setup.py
@@ -5,18 +5,15 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='fdfdpy',
-    version='0.1.0',
+    version='0.1.2',
     description='Electromagnetic Finite Difference Frequency Domain Solver',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='Tyler Hughes, Momchil Minkov, Ian Williamson',
-    author_email='twhughes@stanford.edu',
+    author_email='tylerwhughes91@gmail.com',
     url='https://github.com/fancompute/fdfdpy',
-    license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
           'pyMKL',
@@ -25,4 +22,9 @@ setup(
           'matplotlib',
           'progressbar'
       ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
